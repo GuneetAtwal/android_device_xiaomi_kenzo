e66955a1b8883ae38253476e5e3d090e3e22269a
GuneetAtwal <guneetatwal@gmail.com>
2016-09-05 21:09:25 +0530

    vanirfy
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- vanir_kenzo.mk
+++ vanir_kenzo.mk
@@ -25,7 +25,7 @@ $(call inherit-product, device/xiaomi/kenzo/device.mk)
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := full_kenzo
+PRODUCT_NAME := vanir_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
